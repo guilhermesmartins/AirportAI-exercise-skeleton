70d79b5684ca4368cc2b97ee04397179b75eb677
Guilherme da Silva Martins <mineg4500@gmail.com>
2023-01-31 20:53:46 +0000

    fix(LoggerMiddleware): add underscore to unused variable

--- src/shared/logger/LoggerMiddleware.ts
+++ src/shared/logger/LoggerMiddleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 const logger = getLogger('request-logger');
 
 class LoggerMiddleware {
-  public execute = (req: Request, res: Response, next: NextFunction): void => {
+  public execute = (req: Request, _res: Response, next: NextFunction): void => {
     logger.info(`${req.method} ${req.path}`);
     next();
   };

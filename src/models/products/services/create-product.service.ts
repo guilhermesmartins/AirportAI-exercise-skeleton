30a7420c7d25cc38f1d1bc72adba42e1d1b88905
Guilherme da Silva Martins <mineg4500@gmail.com>
2023-01-31 22:30:35 +0000

    feat: add create product implementation

--- src/models/products/services/create-product.service.ts
+++ src/models/products/services/create-product.service.ts
@@ -1,7 +1,21 @@
-import CreateProductDTO from '../dtos/create-product.dto';
+import { injectable, inject } from 'tsyringe';
+import { InternalServerError } from 'http-errors';
 
+import CreateProductDTO from '../dtos/create-product.dto';
+import { ProductsRepositoryInterface } from '../repositories/products-repository.interface';
+
+@injectable()
 class CreateProductService {
-  async execute(createProductDTO: CreateProductDTO) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: ProductsRepositoryInterface,
+  ) {}
+
+  async execute(createProductDTO: CreateProductDTO) {
+    const product = await this.productsRepository.create(createProductDTO);
+
+    return product;
+  }
 }
 
 export default CreateProductService;

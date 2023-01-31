74503b45cf91e715d890bbaf96ebfa0f32f9a368
Guilherme da Silva Martins <mineg4500@gmail.com>
2023-01-31 21:34:40 +0000

    feat(products): add products routes setup

--- src/models/products/routes/products.routes.ts
+++ src/models/products/routes/products.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { makeValidateBody } from 'express-class-validator';
+
+import CreateProductController from '../controllers/create-product.controller';
+import CreateProductDTO from '../dtos/create-product.dto';
 
 class ProductsRoutes {
   execute() {
     const router = Router();
 
-    router.get('/');
+    const createProductController = new CreateProductController();
+
+    router.post(
+      '/',
+      makeValidateBody(CreateProductDTO),
+      createProductController.execute,
+    );
   }
 }
 

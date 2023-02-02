66cc5468d919f1c7aa4b85abac54408fc0c7119f
Guilherme da Silva Martins <mineg4500@gmail.com>
2023-02-02 13:28:32 +0000

    feat: search by keyword

---
+++ src/models/products/services/find-by-keyword.service.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'tsyringe';
+import { NotFound } from 'http-errors';
+
+import FindByKeywordDTO from '../dtos/find-by-keyword.dto';
+import { ProductsRepositoryInterface } from '../repositories/products-repository.interface';
+
+@injectable()
+class FindByKeywordService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: ProductsRepositoryInterface,
+  ) {}
+
+  async execute(findByKeywordDTO: FindByKeywordDTO) {
+    const products = await this.productsRepository.findByKeywords(
+      findByKeywordDTO,
+    );
+
+    if (!products?.length) {
+      throw new NotFound('No products found matching your criteria');
+    }
+
+    return products;
+  }
+}
+
+export default FindByKeywordService;

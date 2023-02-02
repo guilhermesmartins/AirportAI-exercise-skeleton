208ea633a61792f77c64536df4e4ab014a53090e
Guilherme da Silva Martins <mineg4500@gmail.com>
2023-02-02 09:49:34 +0000

    feat(products): change interface Product import and add findByKeywords

--- src/models/products/repositories/products-repository.interface.ts
+++ src/models/products/repositories/products-repository.interface.ts
@@ -1,13 +1,19 @@
-import { Product } from '@prisma/client';
+import { ProductDocument } from '../entities/product';
 
 import CreateProductDTO from '../dtos/create-product.dto';
 import DeleteProductDTO from '../dtos/delete-product.dto';
 import FindOneProductDTO from '../dtos/find-one-product.dto';
 import FindProductsDTO from '../dtos/find-products.dto';
+import FindByKeywordDTO from '../dtos/find-by-keyword.dto';
 
 export interface ProductsRepositoryInterface {
-  create(createProductDTO: CreateProductDTO): Promise<Product>;
-  findOne(findOneProductDTO: FindOneProductDTO): Promise<Product | undefined>;
-  find(findProductsDTO: FindProductsDTO): Promise<Product[] | undefined>;
-  delete(deleteProductDTO: DeleteProductDTO): Promise<boolean>;
+  create(createProductDTO: CreateProductDTO): Promise<ProductDocument | null>;
+  findOne(
+    findOneProductDTO: FindOneProductDTO,
+  ): Promise<ProductDocument | null>;
+  find(findProductsDTO: FindProductsDTO): Promise<ProductDocument[] | null>;
+  findByKeywords(
+    findByKeywords: FindByKeywordDTO,
+  ): Promise<ProductDocument[] | null>;
+  delete(deleteProductDTO: DeleteProductDTO): Promise<void>;
 }

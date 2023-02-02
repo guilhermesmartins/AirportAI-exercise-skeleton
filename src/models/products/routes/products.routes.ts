cb68caa2ffa3026c73784a77ee9e18a924f435c0
Guilherme da Silva Martins <mineg4500@gmail.com>
2023-02-02 13:28:49 +0000

    feat(products): add validators and new routes

--- src/models/products/routes/products.routes.ts
+++ src/models/products/routes/products.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { makeValidateBody } from 'src/shared/validator';
+import { makeValidateBody, makeValidateParams } from 'src/shared/validator';
 
 import CreateProductController from '../controllers/create-product.controller';
+import DeleteProductController from '../controllers/delete-product.controller';
+import FindByKeywordController from '../controllers/find-by-keyword.controller';
+import FindOneProductController from '../controllers/find-one-product.controller';
 import CreateProductDTO from '../dtos/create-product.dto';
+import DeleteProductDTO from '../dtos/delete-product.dto';
+import FindByKeywordDTO from '../dtos/find-by-keyword.dto';
+import FindOneProductDTO from '../dtos/find-one-product.dto';
 
 class ProductsRoutes {
   execute() {
@@ -14,6 +20,30 @@ class ProductsRoutes {
       '/',
       makeValidateBody(CreateProductDTO),
       createProductController.execute,
+    );
+
+    const findOneProductController = new FindOneProductController();
+
+    router.get(
+      '/:id',
+      makeValidateParams(FindOneProductDTO),
+      findOneProductController.execute,
+    );
+
+    const findByKeywordController = new FindByKeywordController();
+
+    router.post(
+      '/keyword',
+      makeValidateBody(FindByKeywordDTO),
+      findByKeywordController.execute,
+    );
+
+    const deleteProductController = new DeleteProductController();
+
+    router.delete(
+      '/:id',
+      makeValidateParams(DeleteProductDTO),
+      deleteProductController.execute,
     );
 
     return router;
